19d91d102da78a1207b993021446f4d39b9a4bf0
melvincabatuan <melvincabatuan@gmail.com>
2016-06-10 09:05:46 +0800

    latest Android Studio 2.2 in Ubuntu 12.04

---
+++ app/src/main/java/ph/edu/dlsu/latesthelloandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.latesthelloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
